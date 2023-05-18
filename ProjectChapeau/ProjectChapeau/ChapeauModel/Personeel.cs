eb8862cc945234133b687b2b674254d7dd6c5356
Aaron Wesselius <wesseliusaaron@gmail.com>
2023-05-18 16:28:15 +0200

    Eerste deel

---
+++ ProjectChapeau/ProjectChapeau/ChapeauModel/Personeel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectChapeau.ChapeauModel
+{
+    class Personeel
+    {
+        private int id;
+        private string username;
+        private string name;
+        private FunctieType functie;
+        private Wachtwoord wachtwoord;       
+    }
+}
