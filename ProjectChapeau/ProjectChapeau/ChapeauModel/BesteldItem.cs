ef10d1c22092bea7968d3088b745dcf803cdfaba
yusuf-58 <yusufsakarya58@gmail.com>
2023-06-15 15:48:49 +0200

    ...

--- ProjectChapeau/ProjectChapeau/ChapeauModel/BesteldItem.cs
+++ ProjectChapeau/ProjectChapeau/ChapeauModel/BesteldItem.cs
@@ -6,12 +6,9 @@ using System.Threading.Tasks;
 
 namespace ProjectChapeau.ChapeauModel
 {
-    class BesteldItem
     {
-        public int BestedItemId { get; set; }
         public string Opmerking { get; set; }
         public DateTime InstuurTijd { get; set; }
-        public MenuItem menuItemId { get; set; }
         public GerechtsStatus Status { get; set; }
 
     }
